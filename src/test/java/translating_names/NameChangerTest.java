dd1f1bb73bf8143d7fab0d2d77528ee688f298f7
Colin Wakefield <59844720+colinwake@users.noreply.github.com>
2020-02-13 13:32:30 -0500

    Reformatting.
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

--- src/test/java/translating_names/NameChangerTest.java
+++ src/test/java/translating_names/NameChangerTest.java
@@ -8,60 +8,60 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class NameChangerTest {
 
 
-  private NameChanger underTest;
-  private String result;
+    private NameChanger underTest;
+    private String result;
 
-  @BeforeEach
-  void setUp() {
+    @BeforeEach
+    void setUp() {
 
-    underTest = new NameChanger();
-  }
+        underTest = new NameChanger();
+    }
 
-  @Test
-  public void translateToSnakeCase_GivenHelloWorld_Returns_hello_world() {
-    result = underTest.translateToSnakeCase("HelloWorld");
-    assertThat(result).isEqualTo("hello_world");
-  }
+    @Test
+    public void translateToSnakeCase_GivenHelloWorld_Returns_hello_world() {
+        result = underTest.translateToSnakeCase("HelloWorld");
+        assertThat(result).isEqualTo("hello_world");
+    }
 
-  @Test
-  public void translateToSnakeCaseGiven_hello_world_Returns_hello_world() {
-    result = underTest.translateToSnakeCase("hello_world");
-    assertThat(result).isEqualTo("hello_world");
-  }
+    @Test
+    public void translateToSnakeCaseGiven_hello_world_Returns_hello_world() {
+        result = underTest.translateToSnakeCase("hello_world");
+        assertThat(result).isEqualTo("hello_world");
+    }
 
-  @Test
-  public void translateToSnakeCaseGiven_WorldHello_Returns_world_hello() {
-    result = underTest.translateToSnakeCase("WorldHello");
-    assertThat(result).isEqualTo("world_hello");
-  }
+    @Test
+    public void translateToSnakeCaseGiven_WorldHello_Returns_world_hello() {
+        result = underTest.translateToSnakeCase("WorldHello");
+        assertThat(result).isEqualTo("world_hello");
+    }
 
-  @Test
-  public void translateToSnakeCaseGiven_worldHello_Returns_world_hello() {
-    result = underTest.translateToSnakeCase("worldHello");
-    assertThat(result).isEqualTo("world_hello");
-  }
+    @Test
+    public void translateToSnakeCaseGiven_worldHello_Returns_world_hello() {
+        result = underTest.translateToSnakeCase("worldHello");
+        assertThat(result).isEqualTo("world_hello");
+    }
 
-  @Test
-  public void translateToSnakeCaseGiven_hiThere_Returns_hi_there() {
-    result = underTest.translateToSnakeCase("hiThere");
-    assertThat(result).isEqualTo("hi_there");
-  }
+    @Test
+    public void translateToSnakeCaseGiven_hiThere_Returns_hi_there() {
+        result = underTest.translateToSnakeCase("hiThere");
+        assertThat(result).isEqualTo("hi_there");
+    }
 
-  @Test
-  public void translateToSnakeCaseGiven_HiThere_Returns_hi_there() {
-    result = underTest.translateToSnakeCase("HiThere");
-    assertThat(result).isEqualTo("hi_there");
-  }
+    @Test
+    public void translateToSnakeCaseGiven_HiThere_Returns_hi_there() {
+        result = underTest.translateToSnakeCase("HiThere");
+        assertThat(result).isEqualTo("hi_there");
+    }
 
-  @Test
-  public void translateToSnakeCaseGiven_thisCoolString_Returns_this_cool_string() {
-    result = underTest.translateToSnakeCase("thisCoolString");
-    assertThat(result).isEqualTo("this_cool_string");
-  }
+    @Test
+    public void translateToSnakeCaseGiven_thisCoolString_Returns_this_cool_string() {
+        result = underTest.translateToSnakeCase("thisCoolString");
+        assertThat(result).isEqualTo("this_cool_string");
+    }
 
-  @Test
-  public void translateToSnakeCaseGiven_123HelloThere_Returns_123_hello_there() {
-    result = underTest.translateToSnakeCase("123HelloThere");
-    assertThat(result).isEqualTo("123_hello_there");
-  }
+    @Test
+    public void translateToSnakeCaseGiven_123HelloThere_Returns_123_hello_there() {
+        result = underTest.translateToSnakeCase("123HelloThere");
+        assertThat(result).isEqualTo("123_hello_there");
+    }
 }
