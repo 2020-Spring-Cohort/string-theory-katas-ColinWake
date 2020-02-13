c5931f650a5a54706bc7724dceb910dd11629a1f
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-02-13 13:24:11 -0500

    Full test suite and solution for NameChanger
    Signed-off-by: Colin Wakefield <59844720+ColinWake@users.noreply.github.com>

--- src/main/java/translating_names/NameChanger.java
+++ src/main/java/translating_names/NameChanger.java
@@ -2,8 +2,20 @@ package translating_names;
 
 public class NameChanger {
 
-  public String translateToSnakeCase(String name) {
-    return "hello_world";
-  }
+    public String translateToSnakeCase(String name) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : name.toCharArray()) {
+
+            if (ch != name.charAt(0) && Character.isUpperCase(ch)) {
+                sb.append("_").append(ch);
+            } else {
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString().toLowerCase();
+    }
 
 }
